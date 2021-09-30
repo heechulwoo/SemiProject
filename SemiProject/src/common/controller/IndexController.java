c58f75f035dd99ab8f2daf259da1291ee8e157cc
heechulwoo <whc0521@naver.com>
2021-09-30 17:20:25 +0900

    변경

--- SemiProject/src/common/controller/IndexController.java
+++ SemiProject/src/common/controller/IndexController.java
@@ -1,15 +1,10 @@
 package common.controller;
 
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class IndexController extends AbstractController {
 
-/*	
-	@Override
-	public String toString() {
-		return "### 클래스 IndexController 의 인스턴스 메소드 toString() 호출됨 ###";
-	}
-*/
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// IndexController 클래스의 인스턴스 메소드
@@ -20,5 +15,5 @@ public class IndexController extends AbstractController {
 		super.setViewPage("/WEB-INF/index.jsp");
 		
 	}
-	
+
 }
