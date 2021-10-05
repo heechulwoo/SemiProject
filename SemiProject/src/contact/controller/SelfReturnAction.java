5d9c3fc837e6abbff1f1288ddf8fa7c34e633a12
sm408 <sm408@DESKTOP-6GM5SFR>
2021-10-06 02:30:03 +0900

    selfReturn(셀프반품 신청)페이지 추가

---
+++ SemiProject/src/contact/controller/SelfReturnAction.java
@@ -0,0 +1,17 @@
+package contact.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class SelfReturnAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+			//  super.setRedirect(false); // 생략가능
+				super.setViewPage("/WEB-INF/contact/selfReturn.jsp");
+	}
+
+}
