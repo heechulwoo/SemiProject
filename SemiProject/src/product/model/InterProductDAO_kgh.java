742a0202342ddc1f57e4416ae1f453a029774528
user <user@DESKTOP-EP83E72>
2021-10-13 22:21:07 +0900

    productDAO 파일 수정

--- SemiProject/src/product/model/InterProductDAO_kgh.java
+++ SemiProject/src/product/model/InterProductDAO_kgh.java
@@ -1,7 +1,7 @@
 package product.model;
 
 import java.sql.SQLException;
-import java.util.List;
+import java.util.*;
 
 public interface InterProductDAO_kgh {
 
@@ -11,4 +11,10 @@ public interface InterProductDAO_kgh {
 
 	// 상품의 정보를 가져오는 메서드
 	ProductVO_kgh selectProductDetail(String pnum) throws SQLException;
+
+	// 주문번호 확인하는 메서드
+	boolean shippingNoCheck(String shippingNo, String shippingEmail) throws SQLException;
+
+	// 주문번호와 이메일을 통해 주문과 주문 상세를 select 하는 메서드
+	List<ProductOrderDetailVO_kgh> selectOrderConfirmation(String shippingNo, String shippingEmail) throws SQLException;
 }
